0c53f7d05dbdc4fc0de45599969ae352b364995a
Henryk Paluch <henryk.paluch@pickering.cz>
2022-02-23 13:21:58 +0100

    Early native DEB build

---
+++ build_deb.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# direct build of Debian package
+# install
+#   sudo apt-get install devscripts cmake make gcc git
+
+set -euo pipefail
+set -x
+
+cd $(dirname $0)
+# use "date -R" to get date for debian/changelog
+# you may NOT add revision to   debian/changelog
+# or you will get "native package version may not have a revision"
+dpkg-parsechangelog
+# build binary package
+debuild -i -us -uc -b
+# build source package
+debuild -i -us -uc -S
+exit 0
+
